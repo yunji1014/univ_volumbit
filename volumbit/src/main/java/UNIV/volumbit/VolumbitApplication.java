1ec97220e7761aaf5a339bf0f33726c922a5df34
yunji <7787yunji@gmail.com>
2025-05-18 14:00:57 +0900

    초기 세팅

---
+++ volumbit/src/main/java/UNIV/volumbit/VolumbitApplication.java
@@ -0,0 +1,13 @@
+package UNIV.volumbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolumbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolumbitApplication.class, args);
+	}
+
+}
---
+++ volumbit/src/test/java/UNIV/volumbit/VolumbitApplicationTests.java
@@ -0,0 +1,13 @@
+package UNIV.volumbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolumbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
